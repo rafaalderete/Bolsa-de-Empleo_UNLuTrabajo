e8a6677520d60db300612fd223ef58a44a5e73e8
victoriamedina <medina.vicc@gmail.com>
2017-05-06 16:44:39 -0300

    algunas correciones

--- app/Http/Controllers/IdiomasController.php
+++ app/Http/Controllers/IdiomasController.php
@@ -18,7 +18,7 @@ class IdiomasController extends Controller
     {
         #primero debo asegurarme que la persoana que intenta acceder tenga los permisos
         if (Auth::user()->can('listar_idiomas')) {
-            $idiomas = Idiomas::orderBy('id', DESC)->get(); #me traigo de la bd los idiomas cargados en id descendentes
+            $idiomas = Idioma::orderBy('id', DESC)->get(); #me traigo de la bd los idiomas cargados en id descendentes
 
             return view('in.idiomas.index')->with('idiomas', $idiomas);
         } else {
@@ -50,7 +50,7 @@ class IdiomasController extends Controller
     public function store(Request $request)
     {
        if(Auth::user()->can('crear_idiomas')){
-        $idioma = new Idiomas($request->all());
+        $idioma = new Idioma($request->all());
 
         $rubro->save();
 
@@ -81,7 +81,7 @@ class IdiomasController extends Controller
     public function edit($id)
     {
         if(Auth::user()->can('modificar_idiomas')){
-        $idioma = Idiomas::find($id);
+        $idioma = Idioma::find($id);
         return view('in.idiomas.edit')->with('idioma',$idioma);
       }else{
         return redirect()->route('in.sinpermisos.sinpermisos');
@@ -97,8 +97,18 @@ class IdiomasController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+        if(Auth::user()->can('modificar_idiomas')){
+            $idioma = Idioma::find($id);
+
+            $idioma->fill($request->all());
+            $idioma->save();
+
+            Flash::warning('Idioma ' . $idioma->nombre_idioma . ' modificado.')->important();
+            return redirect()->route('in.idiomas.index');
+          }else{
+            return redirect()->route('in.sinpermisos.sinpermisos');
+          }    
+      }
 
     /**
      * Remove the specified resource from storage.
@@ -106,8 +116,25 @@ class IdiomasController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    /*public function destroy($id)
     {
-        //
-    }
+        if(Auth::user()->can('eliminar_idiomas')){
+            $idioma = Idiomas::find($id);
+            $pjuridicas = Juridica::where('rubro_empresarial_id','=',$id)->get();
+            $experiencias_laborales = Experiencia_Laboral::where('rubro_empresarial_id','=',$id)->get();
+            if( (count($pjuridicas) == 0 ) && (count($experiencias_laborales) == 0 ) ) {//Se verifica que no esta uso.
+
+              $rubro->delete();
+
+              Flash::error('Rubro ' . $rubro->nombre_rubro_empresarial . ' eliminado.')->important();
+              return redirect()->route('in.rubros-empresariales.index');
+            }
+            else {
+              Flash::error('El Rubro ' . $rubro->nombre_rubro_empresarial . ' no se puede eliminar ya que se encuentra en uso.')->important();
+              return redirect()->route('in.rubros-empresariales.index');
+            }
+          }else{
+            return redirect()->route('in.sinpermisos.sinpermisos');
+          }
+    }*/
 }
